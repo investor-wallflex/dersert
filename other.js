353080e98f093b8969c3e6c71303f78bf7d56dd3
investor-wallflex <mustaphasharafa70@gmail.com>
2024-10-07 14:38:39 +0100

    jjk

--- other.js
+++ other.js
@@ -1,18 +1,19 @@
 const cart = {
-    productA: { quantity: 0, price: 5.50 },
-    productB: { quantity: 0, price: 4.00 },
-    productC: { quantity: 0, price: 4.50 },
-    productD: { quantity: 0, price: 7.00 },
-    productE: { quantity: 0, price: 8.00 },
-    productF: { quantity: 0, price: 5.00 },
-    productG: { quantity: 0, price: 6.50 },
-    productH: { quantity: 0, price: 5.50 },
-    productI: { quantity: 0, price: 6.50 },
+    productA: {name:'productA', quantity: 0, price: 5.50, image:'addtocart/image-baklava-mobile.jpg', },
+    productB: {name:'productB', quantity: 0, price: 4.00, image:'addtocart/image-baklava-mobile.jpg', },
+    productC: {name:'productC', quantity: 0, price: 4.50, image:'addtocart/image-baklava-mobile.jpg', },
+    productD: {name:'productD', quantity: 0, price: 7.00, image:'addtocart/image-baklava-mobile.jpg', },
+    productE: {name:'productE', quantity: 0, price: 8.00, image:'addtocart/image-brownie-mobile.jpg', },
+    productF: {name:'productF', quantity: 0, price: 5.00, image:'', },
+    productG: {name:'productG', quantity: 0, price: 6.50, image:'', },
+    productH: {name:'productH', quantity: 0, price: 5.50, image:'', },
+    productI: {name:'productI', quantity: 0, price: 6.50, image:'', },
 };
 
 function increment(product, price) {
     cart[product].quantity++;
     updateCart();
+
 }
 
 function decrement(product) {
@@ -22,16 +23,63 @@ function decrement(product) {
     updateCart();
 }
 
+
 function updateCart() {
     let totalPrice = 0;
+    let cartTotal = 0;
+    const cartItems = document.getElementById('cart-items');
+    const emptyCartImage = document.getElementById('empty-cart-image');
+    
+    // clear the cart list before adding new ones
+
+    cartItems.innerHTML = '';
+    
+    // loop through each product in the cart
 
     for (const product in cart) {
         const quantity = cart[product].quantity;
         const productElement = document.getElementById(`${product}-quantity`);
         productElement.innerText = quantity;
-
+        
+        //update cartTotal and totalPrice
         totalPrice += quantity * cart[product].price;
+        cartTotal += quantity;
     }
 
-    document.getElementById('total-price').innerText = totalPrice;
-};
+        document.getElementById('cartTotal').innerText = cartTotal;
+        document.getElementById('total-price').innerText = totalPrice.toFixed(2);
+
+       
+}
+
+
+        
+    
+         /*if (quantity > 0) {
+             const li = document.createElement('li');
+
+             const productName = document.createElement('span');
+             productName.innerText = `${cart[product].name} (x${quantity})`;
+
+             const productImage = document.createElement('img');
+             productImage.scr = cart[product].image;
+             productImage.alt = cart[poduct].name;
+             productImage.style.width = '50px';
+
+             li.appendChild(productImage);
+             li.appendChild(productName);
+
+             cartItems.appendChild(li);
+         }
+     
+        
+    // //document.getElementById('cartTotal').innerText = cartTotal;
+    // document.getElementById('total-price').innerText = totalPrice.toFixed(2)
+
+
+     if (cartTotal === 0) {
+         emptyCartImage.style.display = 'block';
+     } else {
+         emptyCartImage.style.display = 'none';
+     }*/
+       
